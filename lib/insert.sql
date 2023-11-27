4b14b93c4cd6dd46ef2b9c63d334e106f781f39f
Jack Henza <jackhenza@gmail.com>
2023-11-26 20:13:37 -0500

    organize bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',7,'M','brown','aggressive',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',7,'M','brown','aggressive',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha',7,'F','brown','curious',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',7,'M','brown','aggressive',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',7,'M','brown','aggressive',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',7,'M','brown','aggressive',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy',3,'F','black','lethargic',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL,7,'M','brown','aggressive',1);
